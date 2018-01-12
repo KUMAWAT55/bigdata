7e6c2112ee5e908f11d51c9309e49b1231e25a23
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-12 23:14:54 +0530

    source file

--- spark/Scala/UFO/Source.scala
+++ spark/Scala/UFO/Source.scala
@@ -1,1 +1,22 @@
+val InputFile=spark.read.format("com.databricks.spark.csv")
+  .option("header","true")
+  .option("inferschema","true")
+  .load("/FileStore/tables/complete.csv")
+println(InputFile.count())
+  InputFile.createOrReplaceTempView("UFO")
+ val max_country=sqlContext.sql("""select country, cntCountry from (
+                                            select country, count(*) as cntCountry, max(count(*)) over () as maxcnt 
+                                            from UFO 
+                                            group by country) as tmp
+                                            where tmp.cntCountry = tmp.maxcnt""")
+
+
+display(max_country)
+ val max_state=sqlContext.sql("""select state, cntState from (
+                                            select state, count(*) as cntState, max(count(*)) over () as maxcnt 
+                                            from UFO 
+                                            group by state) as tmp
+                                            where tmp.cntState = tmp.maxcnt""")
+
+display(max_state)
 
