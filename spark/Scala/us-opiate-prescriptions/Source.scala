b9fd2d21314fde38b7f183bb10cf22b448fa2ddb
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-24 22:18:01 +0530

    Updated file

--- spark/Scala/us-opiate-prescriptions/Source.scala
+++ spark/Scala/us-opiate-prescriptions/Source.scala
@@ -9,13 +9,22 @@ val Overdoses_temp=spark.read.format("com.databricks.spark.csv")
   .option("inferschema","true")
   .load("/FileStore/tables/overdoses.csv")
 
+
 val Overdoses_temp_2=Overdoses_temp
   .withColumn("Population", regexp_replace(Overdoses_temp("Population"), ",", "").cast("Long"))
   .withColumn("Deaths", regexp_replace(Overdoses_temp("Deaths"), ",", "").cast("Long"))
+  .withColumn("State Name",Overdoses_temp.col("State"))
+  .drop("State")
 
-val Joined_table=Prescriber.join(Overdoses_temp_2,Prescriber("State")===Overdoses_temp_2("Abbrev"),"Inner")
-Joined_table.printSchema
+
+val Joined_table=Prescriber.join(Overdoses_temp_2,Prescriber("State")===Overdoses_temp_2("Abbrev"),"Inner").drop("Abbrev")
 Joined_table.createOrReplaceTempView("Joined_table")
-val Opioid_Prescribers=sqlContext.sql(
-  """ select * from Joined_table where `Opioid.Prescriber`=1"""
-                          )
+val Opioid_Prescribers=sqlContext.sql
+        (
+    """select * from Joined_table where `Opioid.Prescriber`=1 """
+        )
+Opioid_Prescribers.write
+  .mode(SaveMode.Overwrite)
+  .format("com.databricks.spark.csv")
+  .save("/FileStore/tables/Medical_Output/Opioid_Prescriber.csv")
+
