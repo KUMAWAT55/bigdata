93596f935fa171208f37ff72578e1599a572003d
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-24 21:59:35 +0530

    import

--- spark/Scala/us-opiate-prescriptions/Source.scala
+++ spark/Scala/us-opiate-prescriptions/Source.scala
@@ -1,1 +1,10 @@
+import org.apache.spark.sql.functions.regexp_replace
+val Prescriber=spark.read.format("com.databricks.spark.csv")
+  .option("header","true")
+  .option("inferschema","true")
+  .load("/FileStore/tables/prescriber_info-b6d97.csv")
 
+val Overdoses_temp=spark.read.format("com.databricks.spark.csv")
+  .option("header","true")
+  .option("inferschema","true")
+  .load("/FileStore/tables/overdoses.csv")
