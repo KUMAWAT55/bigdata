dbd53f84248a22b8bf50ab4d4484a4af0eaadc7c
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-01-24 22:04:40 +0530

    update

--- spark/Scala/us-opiate-prescriptions/Source.scala
+++ spark/Scala/us-opiate-prescriptions/Source.scala
@@ -8,3 +8,14 @@ val Overdoses_temp=spark.read.format("com.databricks.spark.csv")
   .option("header","true")
   .option("inferschema","true")
   .load("/FileStore/tables/overdoses.csv")
+
+val Overdoses_temp_2=Overdoses_temp
+  .withColumn("Population", regexp_replace(Overdoses_temp("Population"), ",", "").cast("Long"))
+  .withColumn("Deaths", regexp_replace(Overdoses_temp("Deaths"), ",", "").cast("Long"))
+
+val Joined_table=Prescriber.join(Overdoses_temp_2,Prescriber("State")===Overdoses_temp_2("Abbrev"),"Inner")
+Joined_table.printSchema
+Joined_table.createOrReplaceTempView("Joined_table")
+val Opioid_Prescribers=sqlContext.sql(
+  """ select * from Joined_table where `Opioid.Prescriber`=1"""
+                          )
