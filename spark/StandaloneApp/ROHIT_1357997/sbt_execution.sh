a5fbac84969bf6a0aaa94b5f8045fd0c324abb90
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2018-02-05 18:43:26 +0530

    Delete

--- spark/StandaloneApp/ROHIT_1357997/sbt_execution.sh
+++
@@ -1,7 +0,0 @@
-mkdir lib project target
-mkdir -p src/{main,test}/{java,resource,scala}
-echo 'name := "First Project"
-version := "1.0"
-scalaVersion := "2.10.4"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2"
-' >> build.sbt
